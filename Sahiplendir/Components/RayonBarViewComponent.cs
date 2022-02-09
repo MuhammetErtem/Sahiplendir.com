f10720544913ab8c23677f8e30724d81fee2363b
Muhammet ERTEM <mami98_61@hotmail.com>
2022-02-10 01:52:41 +0300

    Ana Sayfa Giriş 2

--- Sahiplendir/Components/RayonBarViewComponent.cs
+++ Sahiplendir/Components/RayonBarViewComponent.cs
@@ -1,9 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sahiplendir.Models;
+using System.Linq;
 
 namespace Sahiplendir.Components
 {
     public class RayonBarViewComponent : ViewComponent
     {
+
+        private readonly AppDbContext context;
+
+        public RayonBarViewComponent(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(context.Rayons.Include(p => p.Categories).Where(p => p.Enabled).ToList());
+        }
 
     }
 }
--- Sahiplendir/Controllers/HomeController.cs
+++ Sahiplendir/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Sahiplendir.Models;
 using System;
@@ -11,15 +12,20 @@ namespace Sahiplendir.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly AppDbContext context;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(AppDbContext context,ILogger<HomeController> logger)
         {
+            this.context = context;
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            ViewBag.FeaturedProducts = await context.Products.Include(p => p.Category).Include(p => p.Brand).Where(p => p.Enabled).OrderBy(p => Guid.NewGuid()).Take(9).ToListAsync();
+            //ViewBag Controllerdaki action veriyi View'e taşımak için kullanılır.
+            ViewBag.Banners = await context.Banners.Where(p => p.Enabled).ToListAsync();
             return View();
         }
 
